22ca48e3f193f5a65bbf2e8d1b722f645c4328d4
Xinyue Sheng <xinyuesheng@Xinyues-MacBook-Pro.local>
2026-07-31 15:20:31 +0100

    make it executable

--- run_needle.sh
+++ run_needle.sh
@@ -3,5 +3,5 @@ export PYTHONPATH=/usr/local/lasaircode/gitrelease/lasair_needle2.0/
 
 cd /usr/local/lasaircode/gitrelease/lasair_needle2.0/
 
-/usr/local/swtools/python/pstc/anaconda3/envs/needle/bin/python /usr/local/lasaircode/gitrelease/lasair_needle2.0/generate_annotator.py
+/home/xs/anaconda3/envs/needle/bin/python /usr/local/lasaircode/gitrelease/lasair_needle2.0/generate_annotator.py
 
