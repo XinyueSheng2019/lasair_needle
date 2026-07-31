ab6be32e9da5329086d18ec32c5cc4d6c4478d43
Xinyue Sheng <xinyuesheng@Xinyues-MacBook-Pro.local>
2026-07-31 14:47:10 +0100

    modify bash location

--- run_needle.sh
+++ run_needle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-export PYTHONPATH=/usr/local/ps1code/gitrelease/lasair_needle2.0/
+export PYTHONPATH=/usr/local/lasaircode/gitrelease/lasair_needle2.0/
 
-cd /usr/local/ps1code/gitrelease/lasair_needle2.0/
+cd /usr/local/lasaircode/gitrelease/lasair_needle2.0/
 
-/usr/local/swtools/python/pstc/anaconda3/envs/needle/bin/python /usr/local/ps1code/gitrelease/lasair_needle2.0/generate_annotator.py
+/usr/local/swtools/python/pstc/anaconda3/envs/needle/bin/python /usr/local/lasaircode/gitrelease/lasair_needle2.0/generate_annotator.py
 
